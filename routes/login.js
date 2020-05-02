851fc51f60bc1d8ea975074f26cd8e222dfbc56e
larturi <lea.arturi@gmail.com>
2020-05-02 18:08:17 -0300

    Backend server finalizado

--- routes/login.js
+++ routes/login.js
@@ -14,86 +14,79 @@ const GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET;
 // Autenticacion por Google
 // ==============================================================
 
-app.post('/google', async (req, res) => {
+app.post('/google', (req, res,next) =>{  
+    var token = req.body.token;  
+    const oAuth2Client = new OAuth2Client(
+        GOOGLE_CLIENT_ID,     
+        GOOGLE_SECRET   
+    );   
+    
+    const ticket = oAuth2Client.verifyIdToken({idToken: token, audience: GOOGLE_CLIENT_ID });   
+    
+    ticket.then(data =>{     
+        // res.status(200).json({       
+        //     ok: true,       
+        //     tiket: data.payload     
+        // });
 
-    var token = req.body.token || '';
+        Usuario.findOne({ email: data.payload.email}, (err, usuarioBD) => {
 
-    const client = new OAuth2Client(GOOGLE_CLIENT_ID, GOOGLE_SECRET);
-    const ticket = client.verifyIdToken({
-        idToken: token,
-        audience: GOOGLE_CLIENT_ID
-    });
-
-    var googleUser = ticket.then(data => {
-        res.status(200).json({
-            ok: true,
-            ticket: data.payload,
-            userid: data.payload.sub
-        });
-    }).catch(err => {
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                mensaje: 'Token no válido',
-                errors: err
-            });
-        }
-    });
-
-    Usuario.findOne({ email: googleuser.email}, (err, usuarioBD) => {
-
-        if (err) {
-            return res.status(500).json({
-                ok: true,
-                mensaje: 'Error al buscar el usuario',
-                errors: err
-            });
-        }
-
-        if (usuarioBD) {
-            if(usuarioBD.google === false) {
-                return res.status(400).json({
+            if (err) {
+                return res.status(500).json({
                     ok: true,
-                    mensaje: 'Debe autenticarse con credenciales (no google)',
+                    mensaje: 'Error al buscar el usuario',
                     errors: err
                 });
+            }
+    
+            if (usuarioBD) {
+                if(usuarioBD.google === false) {
+                    return res.status(400).json({
+                        ok: true,
+                        mensaje: 'Debe autenticarse con credenciales (no google)',
+                        errors: err
+                    });
+                } else {
+                    // Crear token
+                    var token = jwt.sign({ usuario: usuarioBD}, SEED, {expiresIn : 14400});
+                    usuarioBD.password = ':)';
+    
+                    res.status(200).json({
+                        ok: true,
+                        usuarioBD,
+                        token,
+                        id: usuarioBD.id
+                    });
+                }
             } else {
-                // Crear token
-                var token = jwt.sign({ usuario: usuarioBD}, SEED, {expiresIn : 14400});
-                usuarioBD.password = ':)';
+                // El usuario no existe, hay que crearlo
 
-                res.status(200).json({
-                    ok: true,
-                    usuarioBD,
-                    token,
-                    id: usuarioBD.id
+                var googleUser = data.payload;
+    
+                var usuario = new Usuario();
+                usuario.nombre = googleUser.nombre;
+                usuario.email = googleUser.email;
+                usuario.img = googleUser.picture;
+                usuario.google = true;
+                usuario.password = ':)';
+    
+                usuario.save((err, usuarioBD) => {
+                    var token = jwt.sign({ usuario: usuarioBD}, SEED, {expiresIn : 14400});
+    
+                    res.status(200).json({
+                        ok: true,
+                        usuario,
+                        // googleUser,
+                        token,
+                        id: usuario.id
+                    });
                 });
             }
-        } else {
-            // El usuario no existe, hay que crearlo
 
-            var usuario = new Usuario();
-            usuario.nombre = googleUser.nombre;
-            usuario.email = googleUser.email;
-            usuario.img = googleUser.img;
-            usuario.google = true;
-            usuario.password = ':)';
-
-            usuario.save((err, usuarioBD) => {
-                var token = jwt.sign({ usuario: usuarioBD}, SEED, {expiresIn : 14400});
-
-                res.status(200).json({
-                    ok: true,
-                    usuarioBD,
-                    token,
-                    id: usuarioBD.id
-                });
-            });
-        }
-
+        });
     });
-
 });
+
 
 
 // ==============================================================
